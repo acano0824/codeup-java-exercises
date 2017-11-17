3d356e26a6404146c13bfe6ca76aa96808491ab2
ashleycano <acano6835@gmail.com>
2017-11-17 15:26:40 -0600

    adding solution for 1.a

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
-
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-
+        int i = 5;
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
 
     }
 }
