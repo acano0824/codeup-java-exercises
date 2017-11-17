7ad133879b53b35abd2e8d5dcd3c4885e5f064ee
ashleycano <acano6835@gmail.com>
2017-11-17 15:12:07 -0600

    adding exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        String firstWord, secondWord, thirdWord;
+       Scanner scan = new Scanner(System.in);
+
+    //Enter an Integer
+        System.out.println("Hello! Please input a number!");
+        int userNumber = scan.nextInt();
+
+        System.out.println("I like the number " + userNumber + "!");
+
+        //Enter 3 words
+//        System.out.println("Hello! Please input your three favourite words!");
+//        firstWord = scan.next();
+//        secondWord = scan.next();
+//        thirdWord = scan.next();
+//
+//        System.out.println(firstWord);
+//        System.out.println(secondWord);
+//        System.out.println(thirdWord);
+
+        //Enter a sentence
+//        String sentence;
+//        System.out.println("Hey! What's your favourite quote?");
+//
+//        //a space is the delimeter. Cuts off anything after white space.
+////        sentence = scan.next();
+//
+//        sentence = scan.nextLine();
+//
+//        System.out.println(sentence + " I love that quote!");
+
+//        String Length;
+//        String Width;
+//
+//        System.out.println("Please input the Length and Width of this Classroom.");
+//        Length = scan.nextLine();
+//        Width = scan.nextLine();
+//
+//        System.out.println("area = " + Length + " x " + Width);
+//        System.out.println("perimeter = 2 x " + Length + " + 2 x " + Width);
+
+
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,41 @@
+public class HelloWorld {
+    public static void main(String[] args){
+
+//        int myFavouriteNumber = 8;
+//        System.out.println(myFavouriteNumber);
+//
+//        String myString = "This is my favourite number!";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        class
+//
+//        int x = 4;
+//
+//          x += 5;
+//
+//
+//
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        y *= x;
+//
+//
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        x /= y;
+//
+//        y -= x;
+    }
+}
