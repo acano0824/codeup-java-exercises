fb6b614e4b5ae0cbe6cb74bc39decd86d30dac54
ashleycano <acano6835@gmail.com>
2017-11-21 12:26:36 -0600

    adding basic arithmetic

---
+++ src/MethodsExercises.java
@@ -0,0 +1,80 @@
+import java.util.Scanner;
+//class Add {
+//    public static void main(String[] arg) {
+//        int a, b, c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your first number");
+//        a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        b = sc.nextInt();
+//        c = addition(a, b);
+//        System.out.println("Addition of these two numbers is : " + c);
+//    }
+//    static int addition(int x,int y) {
+//        return x + y;
+//    }
+//}
+
+//class Subtract {
+//    public static void main(String[] arg) {
+//        int a, b, c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your first number");
+//        a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        b = sc.nextInt();
+//        c = subtraction(a, b);
+//        System.out.println("Subtraction of these two numbers is : " + c);
+//    }
+//    static int subtraction(int x,int y) {
+//        return x - y;
+//    }
+//}
+
+//class Multiply {
+//    public static void main(String[] arg) {
+//        int a, b, c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your first number");
+//        a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        b = sc.nextInt();
+//        c = multiplication(a, b);
+//        System.out.println("Multiplication of these two numbers is : " + c);
+//    }
+//    static int multiplication(int x,int y) {
+//        return x * y;
+//    }
+//}
+
+//class Divide {
+//    public static void main(String[] arg) {
+//        int a, b, c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your first number");
+//        a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        b = sc.nextInt();
+//        c = division(a, b);
+//        System.out.println("Division of these two numbers is : " + c);
+//    }
+//    static int division(int x,int y) {
+//        return x / y;
+//    }
+//}
+
+//class Modulus {
+//    public static void main(String[] arg) {
+//        int a, b, c;
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter your first number");
+//        a = sc.nextInt();
+//        System.out.println("Enter your second number");
+//        b = sc.nextInt();
+//        c = modulus(a, b);
+//        System.out.println("Modulus of these two numbers is : " + c);
+//    }
+//    static int modulus(int x,int y) {
+//        return x % y;
+//    }
+//}
