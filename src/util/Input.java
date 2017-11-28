8b8873a35e6010fd41b358ef2e715b8d706e2e32
ashleycano <acano6835@gmail.com>
2017-11-28 10:32:36 -0600

    added solutions to circle

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,6 +3,10 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+    public static void main(String[] args) {
+
+
+    }
 
     private Scanner scanner;
 
@@ -10,23 +14,33 @@ public class Input {
         return scanner.nextLine();
     }
 
-    public boolean yesNo(String userInput) {
-        if (userInput.equalsIgnoreCase("y" || userInput.equalsIgnoreCase("yes"))) {
-
-        } else {
-
-        }
-    }
-
-    public int getInt (int min, int max) {
-
+    public boolean yesNo() {
+       String userChoice = scanner.next();
+        return userChoice.equalsIgnoreCase("y") || userChoice.equalsIgnoreCase("yes");
     }
 
     public int getInt() {
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else {
+            System.out.println("Not an integer!");
+            scanner.next();
+            return getInt();
+        }
+    }
 
+    public int getInt(int min, int max) {
+        int userInput = getInt();
+        if (userInput >= min && userInput <=max) {
+
+        } else {
+            System.out.println("Not in Range!");
+            return getInt(min, max);
+        }
     }
 
     public double getDouble(double min, double max) {
+
 
     }
 
