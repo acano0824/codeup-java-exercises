52b6e109c52fb90f63d231f551e4a466cf0a24d6
ashleycano <acano6835@gmail.com>
2017-11-29 14:16:53 -0600

    adding solution to exercises

---
+++ src/shapes/Rectangle.java
@@ -0,0 +1,24 @@
+package shapes;
+
+public class Rectangle {
+
+    private int length;
+    private int width;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public int getArea() {
+        return this.length * this.width;
+    }
+    public int getPerimeter() {
+        return 2 * this.length + 2 * this.width;
+    }
+
+}
